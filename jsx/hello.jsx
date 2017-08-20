fe13e46a92422a64274ed265028fca79ea75464c
GuyZwer <guy.zwerdling@gmail.com>
2017-08-20 14:58:40 +0300

    add some file

--- jsx/hello.jsx
+++ jsx/hello.jsx
@@ -1,5 +1,10 @@
-class _Main {
-    static function main(args : string[]) : void {
-        log "Hello, world!";
-    }
-}
+var s1 : string;
+s1 = "abc";       // ok
+s1 = null;        // compile error!  cannot assign null to string
+
+var s2 : Nullable.<string>;
+s2 = "abc";       // ok
+s2 = null;        // ok
+
+log s1
+log s2
